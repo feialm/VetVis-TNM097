df53851db6f9bb958a22fcf650bd585e8bbc1292
feialm <45712644+feialm@users.noreply.github.com>
2022-09-08 14:52:43 +0200

    Update scalartocolormapping.cpp

--- tnm067lab1/utils/scalartocolormapping.cpp
+++ tnm067lab1/utils/scalartocolormapping.cpp
@@ -13,10 +13,30 @@ vec4 ScalarToColorMapping::sample(float t) const {
 
     // TODO: use t to select which two base colors to interpolate in-between
 
+    
+    // plocka ut två färger från basecolors, med t
+    // få hela storleken av baseColors_
+    // t som index
 
+    // RGB
+    // 5 färger men size är 4
+
+    float new_t = t * (baseColors_.size() - 1);
+
+    // interpolate those two values
+    int rightIndex = ceil(new_t);
+    int leftIndex = floor(new_t);
+    
+    vec4 col_R = baseColors_[rightIndex];
+    vec4 col_L = baseColors_[leftIndex];
+
+    // linear interpolate
+    // 1 - något
+    
     // TODO: Interpolate colors in baseColors_ and set dummy color to result
 
-    vec4 finalColor(t, t, t, 1);  // dummy color
+    //vec4 finalColor(t, t, t, 1);  // dummy color
+    vec4 finalColor = (col_R - col_L) * (new_t-col_L)+col_L;
        
     return finalColor;
 }
