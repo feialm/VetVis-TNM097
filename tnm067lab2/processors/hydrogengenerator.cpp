b8c35fe07372c5346fa0614ab78f77b5ff3ccad5
feialm <45712644+feialm@users.noreply.github.com>
2022-09-13 18:54:29 +0200

    Update hydrogengenerator.cpp

--- tnm067lab2/processors/hydrogengenerator.cpp
+++ tnm067lab2/processors/hydrogengenerator.cpp
@@ -5,6 +5,9 @@
 #include <inviwo/core/util/indexmapper.h>
 #include <inviwo/core/datastructures/volume/volumeram.h>
 #include <modules/base/algorithm/dataminmax.h>
+#include <cmath>
+#define _USE_MATH_DEFINES
+#include <math.h>
 
 namespace inviwo {
 
@@ -44,8 +47,19 @@ void HydrogenGenerator::process() {
 
 vec3 HydrogenGenerator::cartesianToSpherical(vec3 cartesian) {
     vec3 sph{cartesian};
-
+    //vec3=(x,y,z) [1,2,3]
     // TASK 1: implement conversion using the equations in the lab script
+    sph.r = std::sqrt(std::pow(cartesian.x,2.0) + std::pow(cartesian.y,2.0) + std::pow(cartesian.z,2.0));
+
+    if (sph.r < 1.0e-20) { // Så vi inte delar med 0
+        sph.t = 0.0;
+        sph.p = 0.0;
+        
+        return sph;
+    }
+    sph.t = std::acos(cartesian.z / sph.r);
+    sph.p = std::atan2(cartesian.y, cartesian.x); // kan ge lite olikt resultat
+
 
     return sph;
 }
@@ -54,8 +68,21 @@ double HydrogenGenerator::eval(vec3 cartesian) {
     const double density = cartesian.x;
 
     // TASK 2: Evaluate wave function
+    float Z = 1.f;
+    float a0 = 1.f;
 
-    return density;
+    vec3 sph = cartesianToSpherical(cartesian);
+
+    float yellow = 1.f / (81.f * std::sqrt(6.f * M_PI));
+    float red = pow((Z/a0),(3.f /2.f));
+    float blue = (pow(Z,2.f)*pow(sph.r, 2.f))/pow(a0,2.f);
+    float green = exp((-Z*sph.r)/(3.f*a0));
+    float pink = 3.f*pow(cos(sph.t),2.f)-1.f;
+
+    // QUESTION : SHOULD BE USE DNESITY? WHY CONST?
+
+
+    return pow(yellow * red * blue * green * pink,2.f);
 }
 
 vec3 HydrogenGenerator::idTOCartesian(size3_t pos) {
